9523a8e9352fe6035593f273b49a7b27a873df49
shaharsol <shahar@gnift.com>
2023-03-14 22:56:03 +0200

    web servers

---
+++ 0006-basic-web-server/server-csv-file.js
@@ -0,0 +1,23 @@
+const { createServer } = require('http');
+
+const HOST = 'localhost';
+const PORT = '8080';
+
+const requestListener = function (req, res) {
+    res.setHeader('Content-Type', 'text/csv');
+    res.setHeader("Content-Disposition", "attachment;filename=johnbryce.csv");
+    res.writeHead(200);
+    res.end('id,name,age\n12345678,shahar,47');
+};
+
+const server = createServer(requestListener);
+
+server.listen(PORT, HOST, () => {
+    console.log(`Server is running on http://${HOST}:${PORT}`);
+});
+
+
+// hint
+res.setHeader("Content-Disposition", "attachment;filename=johnbryce.csv");
+
+
