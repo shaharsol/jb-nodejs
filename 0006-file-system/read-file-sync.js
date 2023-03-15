a83bb833c6b5dd54cb556fc49202c341a6d811c3
shaharsol <shahar@gnift.com>
2023-03-15 23:10:26 +0200

    alota

---
+++ 0006-file-system/read-file-sync.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+console.log(fs.readFileSync('content.txt', 'utf8'));
+
+// hints
+readFile('file name', 'encoding', callback)
+const fs = require('fs/promises')
+readFileSync('file name', 'encoding')
+
+
+
---
+++ 0008-npm/random-bitly-string.js
@@ -0,0 +1,10 @@
+const letters = ['a','b','c','d'];
+const rand = Math.round(Math.random(3));
+
+console.log(letters[rand]);
+
+// hint
+Math.random()
+Math.round()
+
+
