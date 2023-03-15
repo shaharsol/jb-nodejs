45475bda3e4132c6a73e32737fb22b59ff9f2177
shaharsol <shahar@gnift.com>
2023-03-15 12:16:15 +0200

    more server examples

---
+++ 0006-basic-web-server/server-multi-endpoints.js
@@ -0,0 +1,29 @@
+const { createServer } = require('http');
+
+const HOST = 'localhost';
+const PORT = '8080';
+
+const requestListener = function (req, res) {
+    console.log(req);
+    if (req.url === '/name') {
+        res.writeHead(200);
+        return res.end("name endpoint");
+    }
+
+    if (req.url === '/age') {
+        res.writeHead(200);
+        return res.end("age endpoint");
+    }
+
+    res.writeHead(404);
+    return res.end("not found");
+};
+
+const server = createServer(requestListener);
+
+server.listen(PORT, HOST, () => {
+    console.log(`Server is running on http://${HOST}:${PORT}`);
+});
+
+// hint
+console.log(req);
