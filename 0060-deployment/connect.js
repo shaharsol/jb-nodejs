b1d5167315965129bac8b90f6a6b6991c46b2947
shaharsol <shahar@gnift.com>
2023-03-19 23:53:09 +0200

    deployment

---
+++ 0060-deployment/connect.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
