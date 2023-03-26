22e1e4efd03df9007055a98c053c22caaa80d2f3
shaharsol <shahar@gnift.com>
2023-03-26 09:28:18 +0300

    views

--- 0007-basic-web-server/server.js
+++ 0007-basic-web-server/server.js
@@ -1,6 +1,6 @@
 const http= require('http');
 
-const HOST = 'localhost';
+const HOST = '127.0.0.1';
 const PORT = '8080';
 
 const requestListener = function (req, res) {
@@ -10,6 +10,6 @@ const requestListener = function (req, res) {
 
 const server = http.createServer(requestListener);
 
-server.listen(PORT, HOST, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on http://${HOST}:${PORT}`);
 });
