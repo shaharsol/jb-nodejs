8f9a27d2d02388f3f65617d32f9dfba57ebec487
shaharsol <shahar@gnift.com>
2023-04-03 15:37:59 +0300

    remove the try/catch from db

--- 0100-app/middlewares/db.js
+++ 0100-app/middlewares/db.js
@@ -3,29 +3,23 @@ const config = require('config');
 const util = require('util');
 const { db } = require('../models/mongo/symbol-value');
 
-let pool;
-
-try {
-    pool = mysql.createPool({
-        host: config.get('mysql.host'),
-        user: config.get('mysql.user'),
-        password: config.get('mysql.password'),
-        database: config.get('mysql.database'),
-        port: config.get('mysql.port'),
-        connectionLimit: 10,
-        waitForConnections: true,
-        maxIdle: 10,
-        idleTimeout: 60000,
-        queueLimit: 0,
-    }) 
+const pool = mysql.createPool({
+    host: config.get('mysql.host'),
+    user: config.get('mysql.user'),
+    password: config.get('mysql.password'),
+    database: config.get('mysql.database'),
+    port: config.get('mysql.port'),
+    connectionLimit: 10,
+    waitForConnections: true,
+    maxIdle: 10,
+    idleTimeout: 60000,
+    queueLimit: 0,
+}) 
 
 
-    pool.query = util.promisify(pool.query);
-    pool.execute = util.promisify(pool.execute);
+pool.query = util.promisify(pool.query);
+pool.execute = util.promisify(pool.execute);
 
-} catch (err) {
-    console.log(err);
-}
 const middleware = (req, res, next) => {
     req.db = pool;
     return next();
