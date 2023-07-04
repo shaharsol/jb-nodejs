5eca81fbce292a8dfa00970dbe3a4589efdb414b
shaharsol <shahar@gnift.com>
2023-07-04 15:02:01 +0300

    basic middlewares

--- 0888-harel/app.js
+++ 0888-harel/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const port = '3000';
 
+// middlewares
+const notFound = require('./middlewares/404')
+const errorHandler = require('./middlewares/error')
+
+// 
 const guestsRoute = require('./routes/guests');
 const usersRoute = require('./routes/users');
 const githubRoute = require('./routes/github');
@@ -10,6 +15,9 @@ const app = express();
 app.use('/', guestsRoute);
 app.use('/', usersRoute);
 app.use('/github', githubRoute);
+
+app.use(errorHandler);
+app.use(notFound);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
