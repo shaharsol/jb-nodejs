dc7d81e3c65d2c7822115a902effd3f1e059a266
shaharsol <shahar@gnift.com>
2023-07-11 10:04:26 +0300

    beautify

--- 0888-harel/middlewares/input-validation.js
+++ 0888-harel/middlewares/input-validation.js
@@ -7,7 +7,10 @@ module.exports = (validator) => async (req, res, next) => {
     //* Pass err to next
     //! If validation error occurs call next with HTTP 422. Otherwise HTTP 500
     if (err.isJoi) { 
-      return next({status: 422, message: err.message }); 
+      return next({
+        status: 422, 
+        message: err.message 
+      }); 
     }
     return next({
       status: 500,
